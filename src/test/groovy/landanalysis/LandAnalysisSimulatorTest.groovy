00bb23e7be02c0ef2e212ef7a1e9302ef79d6165
Mark Howard <mark@dendryte.org>
2016-07-30 19:54:31 -0500

    added more test cases. so far so good

--- src/test/groovy/landanalysis/LandAnalysisSimulatorTest.groovy
+++ src/test/groovy/landanalysis/LandAnalysisSimulatorTest.groovy
@@ -68,6 +68,48 @@ class LandAnalysisSimulatorTest extends Specification {
         fertileAreas.get(1) == 115200;
     }
 
+    def "one rectangle in middle of farm"() {
+        given:
+        farm.addBarrenPlot(new Coordinate(100,100), new Coordinate(199,199));
+
+        when:
+        List<Integer> fertileAreas = landAnalysisSimulator.findFertileAreas();
+
+        then:
+        fertileAreas.size() == 1;
+        fertileAreas.get(0) == 230000;
+    }
+
+    def "two rectangles which intersect in upper right corner of farm"() {
+        given:
+        farm.addBarrenPlot(new Coordinate(300,500), new Coordinate(310,599));
+        farm.addBarrenPlot(new Coordinate(300,500), new Coordinate(399,510));
+
+        when:
+        List<Integer> fertileAreas = landAnalysisSimulator.findFertileAreas();
+
+        then:
+        fertileAreas.size() == 2;
+        fertileAreas.get(0) == 7921;
+        fertileAreas.get(1) == 230000;
+    }
+
+    def "two rectangles which intersect in upper right corner of farm and one rectangle which cuts farm vertically"() {
+        given:
+        farm.addBarrenPlot(new Coordinate(300,500), new Coordinate(310,599))
+        farm.addBarrenPlot(new Coordinate(300,500), new Coordinate(399,510))
+        farm.addBarrenPlot(new Coordinate(100,0), new Coordinate(110,599))
+
+        when:
+        List<Integer> fertileAreas = landAnalysisSimulator.findFertileAreas()
+
+        then:
+        fertileAreas.size() == 3
+        fertileAreas.get(0) == 7921
+        fertileAreas.get(1) == 60000
+        fertileAreas.get(2) == 163400
+    }
+
     def "hash pattern in middle of farm"() {
         given:
         farm.addBarrenPlot(new Coordinate(48,192), new Coordinate(351,207));
@@ -83,5 +125,30 @@ class LandAnalysisSimulatorTest extends Specification {
         fertileAreas.get(0) == 22816;
         fertileAreas.get(1) == 192608;
     }
+
+    def "2 hash patterns in middle of farm"() {
+        given:
+        //first hash
+        farm.addBarrenPlot(new Coordinate(48,92), new Coordinate(351,107));
+        farm.addBarrenPlot(new Coordinate(48,192), new Coordinate(351,207));
+        farm.addBarrenPlot(new Coordinate(120,52), new Coordinate(135,247));
+        farm.addBarrenPlot(new Coordinate(260,52), new Coordinate(275,247));
+        //second hash
+        farm.addBarrenPlot(new Coordinate(48,292), new Coordinate(351,307));
+        farm.addBarrenPlot(new Coordinate(48,392), new Coordinate(351,407));
+        farm.addBarrenPlot(new Coordinate(120,267), new Coordinate(135,447));
+        farm.addBarrenPlot(new Coordinate(260,267), new Coordinate(275,447));
+
+
+        when:
+        List<Integer> fertileAreas = landAnalysisSimulator.findFertileAreas();
+
+        then:
+        fertileAreas.size() == 3;
+        fertileAreas.get(0) == 10416;
+        fertileAreas.get(1) == 10416;
+        fertileAreas.get(2) == 189696;
+    }
+
 
 }
